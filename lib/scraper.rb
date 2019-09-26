99b5c0a3c0a7ee5f044ca9e9f5bab76466ada763
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-26 15:17:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,6 +8,7 @@ class Scraper
   
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    binding.pry
   end 
   
   def get_courses 
@@ -30,5 +31,5 @@ class Scraper
   
 end
 
-
+Scraper.new.get_page
 
