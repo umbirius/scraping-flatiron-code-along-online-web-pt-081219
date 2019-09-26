7832022221b25327fe234e26167b4d99290026c4
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-26 16:16:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -24,7 +24,7 @@ class Scraper
   end 
   
   def make_courses
-    doc.css(".post").each do |post|
+    self.get_courses.each do |post|
       course = Course.new
       course.title = post.css("h2").text 
       course.schedule = post.css(".date").text 
