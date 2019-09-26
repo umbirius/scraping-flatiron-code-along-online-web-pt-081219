06e58dd94f43cfaea062c1953c5f0a97ada3fe6b
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-26 15:09:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,16 @@ require_relative './course.rb'
 
 class Scraper
   
+  def get_page
+  end 
+  
+  def get_courses 
+  end 
+  
+  def make_courses
+  end 
+  
+  
   def print_courses
     self.make_courses
     Course.all.each do |course|
